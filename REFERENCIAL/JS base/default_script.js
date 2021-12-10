0b6b3c6af66101635eb544f8819263796bd24c6d
WilkerSebastianDev <93801669+WilkerSebastianDev@users.noreply.github.com>
2021-12-10 00:03:06 -0400

    commit

---
+++ REFERENCIAL/JS base/default_script.js
@@ -0,0 +1,15 @@
+function abrefecha(elemento) {
+
+  let ex = document.getElementById(elemento);
+
+  if (ex.style.display == "none") {
+
+    ex.style.display = "block";
+
+  } else {
+
+    ex.style.display = "none";
+
+  }
+
+}
